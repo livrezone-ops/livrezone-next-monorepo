47f7f5827156f97a66d2286d357114698ce5d2ec
root <root@casaos-server.casaos-server>
2026-08-14 21:59:18 +0100

    chore: suppression fichiers debug public

--- api-next.livrezone.com/public/debug4.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
- = curl_init();
-curl_setopt(, CURLOPT_URL, "https://api-next.livrezone.com/debug2.php");
-curl_setopt(, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt(, CURLOPT_SSL_VERIFYPEER, false);
- = curl_exec();
-curl_close();
-echo ;
