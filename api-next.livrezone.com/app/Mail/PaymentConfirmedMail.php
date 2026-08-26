7fa07d253ab078b912ae1735a2b6e062c01af8e1
root <root@casaos-server.casaos-server>
2026-08-26 22:43:33 +0100

    mails asynchrones: ShouldQueue sur les 3 mailables + SCOUT_QUEUE=database

--- api-next.livrezone.com/app/Mail/PaymentConfirmedMail.php
+++ api-next.livrezone.com/app/Mail/PaymentConfirmedMail.php
@@ -3,12 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PaymentConfirmedMail extends Mailable
+class PaymentConfirmedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
